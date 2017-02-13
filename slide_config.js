0d7009c3544a1a367c9173447c666bbdb1a62184
Santiago Saavedra <ssaavedra@openshine.com>
2017-02-13 01:17:07 +0100

    Initial commit

---
+++ slide_config.js
@@ -0,0 +1,26 @@
+var SLIDE_CONFIG = {
+   // Slide Settings
+   settings: {
+     title: 'TensorFlow in NLP', 
+     subtitle: 'An Introduction', 
+     useBuilds: true, // Default: true. False will turn off slide animation builds. 
+     usePrettify: true, // Default: true 
+     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+     favIcon: 'images/tensorflow-logo.jpg', 
+     fonts: [
+       'Open Sans:regular,semibold,italic,italicsemibold',
+       'Source Code Pro'
+     ],
+   }, 
+ 
+   // Author information
+   presenters: [{
+     name: 'Santiago Saavedra', 
+     company: 'OpenShine', 
+     gplus: 'https://plus.google.com/+SantiagoSaavedra', 
+     twitter: 'ssice', 
+     www: 'https://ssaavedra.github.io', 
+     github: 'https://github.com/ssaavedra', 
+   }]
+};
