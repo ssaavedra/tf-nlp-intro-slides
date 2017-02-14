6a8130d894dd2542b0253a732cb8b4597ac5e1ac
Santiago Saavedra <ssaavedra@openshine.com>
2017-02-15 00:29:31 +0100

    Push presentation content

--- slide_config.js
+++ slide_config.js
@@ -7,7 +7,7 @@ var SLIDE_CONFIG = {
      usePrettify: true, // Default: true 
      enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
      enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-     favIcon: 'images/tensorflow-logo.jpg', 
+     favIcon: 'images/openshine-icon.svg', 
      fonts: [
        'Open Sans:regular,semibold,italic,italicsemibold',
        'Source Code Pro'
